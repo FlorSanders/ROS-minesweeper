616445890f7e1dc9801eb3ca8432663ea2b50a8b
Flor Sanders <flor.sanders@ugent.be>
2020-12-06 15:37:39 +0100

    Launching nodes YEAHHHH

--- src/minesweeper_package/scripts/nodelauncher.py
+++ src/minesweeper_package/scripts/nodelauncher.py
@@ -2,9 +2,25 @@
 
 # Importing required libraries
 import roslaunch
+import rospy as ros
+
+package = 'minesweeper_package'
+executable = 'random_roomba'
 
 def launchnode():
-    pass
+    # Launching a node
+    node = roslaunch.core.Node(package, executable)
+
+    launcher = roslaunch.scriptapi.ROSLaunch()
+    launcher.start()
+
+    process = launcher.launch(node)
+    print(process.is_alive())
+    ros.sleep(30)
+    process.stop()
+    ros.sleep(10)
+
+    print("Shutting down the script")
 
 
 if __name__ == "__main__":
