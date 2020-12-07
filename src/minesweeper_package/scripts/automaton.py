96e1e46a84c9a2d56146b484479ee389b41ca940
Flor Sanders <flor.sanders@ugent.be>
2020-12-07 14:01:32 +0100

    Started automaton

--- src/minesweeper_package/scripts/automaton.py
+++ src/minesweeper_package/scripts/automaton.py
@@ -5,6 +5,7 @@ import roslaunch
 import rospkg
 import rospy as ros
 from geometry_msgs.msg import PoseWithCovarianceStamped
+import os
 
 def launch_simulation(strategy, environment, duration=30, repetitions=10):
     """
@@ -17,6 +18,7 @@ def launch_simulation(strategy, environment, duration=30, repetitions=10):
     - 0_BasicMinefield
     - 1_BasicMinefield_Small
     - 2_EnhancedMinefield_
+    ...
     """
 
     # Create a ros "node" for this process, launching all the other stuff
@@ -26,63 +28,30 @@ def launch_simulation(strategy, environment, duration=30, repetitions=10):
 
     # Obtaining the path of the launch files we want to initiate
     packager = rospkg.RosPack()
-    gazebo_path = f"{packager.get_path('turtlebot3_gazebo')}/launch/3_EnhancedMinefield_Small.launch"
+    gazebo_path = f"{packager.get_path('turtlebot3_gazebo')}/launch/{environment}.launch"
+    driver_path = f"{packager.get_path('minesweeper_package')}/launch/{strategy}.launch"
+
+    # Making launchers for the nodes
+    gazebo_launcher = roslaunch.parent.ROSLaunchParent(uuid, [gazebo_path])
+    driver_launcher = roslaunch.parent.ROSLaunchParent(uuid, [driver_path])
+
+    for _ in range(repetitions):
+        # Launch the gazebo world and wait a couple of seconds
+        gazebo_launcher.start()
+        ros.sleep(5)
+
+        # Launch the robot and sleep until it's shutdown time
+        driver_launcher.start()
+        ros.sleep(60*duration)
+
+        # Shutdown both launchers and wait a couple of seconds
+        driver_launcher.shutdown()
+        gazebo_launcher.shutdown()
+        ros.sleep(10)
+    
 
 
-# Mapping stage: Build a map of the unknown environment!
-def mapping_stage(uuid):
-    """
-    Function that launches the correct nodes to build the map
-    """
-
-    ## Mapping stage 1: Quickly start building a map by driving away randomly (still using obstacle avoidance)
-
-    # Finding the paths for the launch files we want to run
-    packager = rospkg.RosPack()
-    driver_path = f"{packager.get_path('minesweeper_package')}/launch/random_roomba.launch"
-    slam_path = f"{packager.get_path('turtlebot3_slam')}/launch/turtlebot3_gmapping.launch"
-
-    # Creating launchers for the nodes in this stage
-    driver_launcher = roslaunch.parent.ROSLaunchParent(uuid = roslaunch.rlutil.get_or_generate_uuid(None, False), [driver_path])
-    slam_launcher = roslaunch.parent.ROSLaunchParent(uuid = roslaunch.rlutil.get_or_generate_uuid(None, False), [slam_path])
-
-    # Launching the nodes
-    driver_launcher.start()
-    slam_launcher.start()
-    ros.sleep(5)
-    driver_launcher.shutdown()
-
-    ## Mapping stage 2: Use the explore package to complete the map
-
-    # Finding the paths of the nodes needed for the exploration
-    navigation_path = f"{packager.get_path('turtlebot3_navigation')}/launch/move_base.launch"
-    explorer_path = f"{packager.get_path('explore_lite')}/launch/explore.launch"
-
-    # Creating launchers for the nodes in this stage
-    launcher2 = roslaunch.parent.ROSLaunchParent(uuid = roslaunch.rlutil.get_or_generate_uuid(None, False), [navigation_path, explorer_path])
-    launcher2.start()
-
-    ros.spin()
-
-
-
-def launchnode():
-    ## First launch gazebo and fake
-
-    # Initializing a ros node for this script
-    ros.init_node('NodeLauncher', anonymous=True)
-    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-    roslaunch.configure_logging(uuid)
-
-    packager = rospkg.RosPack()
-    gazebo_path = f"{packager.get_path('turtlebot3_gazebo')}/launch/3_EnhancedMinefield_Small.launch"
-    gazebo_launcher = roslaunch.parent.ROSLaunchParent(uuid = roslaunch.rlutil.get_or_generate_uuid(None, False), [gazebo_path])
-    gazebo_launcher.start()
-
-    ros.sleep(5)
-
-    mapping_stage(uuid)
 
 
 if __name__ == "__main__":
-    launchnode()
+    launch_simulation('random_roomba', '0_BasicMinefield', duration=1, repetitions=2)
